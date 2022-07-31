5ef57701d4b9b068adca724c478fe2a870d8b98c
kimi <kimi.lai@tpisoftware.com>
2022-07-31 09:55:24 +0800

    chore: 任務 01：建立並設定路由機制

---
+++ src/app/posts/posts.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CreateComponent } from './create/create.component';
+import { PostComponent } from './post/post.component';
+import { PostsRoutingModule } from './posts-routing.module';
+import { PostsComponent } from './posts.component';
+
+@NgModule({
+  declarations: [PostsComponent, PostComponent, CreateComponent],
+  imports: [CommonModule, PostsRoutingModule],
+})
+export class PostsModule {}
