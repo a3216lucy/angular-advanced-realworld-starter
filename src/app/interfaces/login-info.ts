4c41edd7efbaa5b59393987f4fd8f0838a85321f
kimi <kimi.lai@tpisoftware.com>
2022-07-31 14:59:56 +0800

    chore: 任務 04：在程式中動態切換路由

--- src/app/interfaces/login-info.ts
+++ src/app/interfaces/login-info.ts
@@ -1,4 +1,4 @@
 export interface UserLoginInfo {
   email: string;
   password: string;
-};
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLoginInfo } from '../interfaces/login-info';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  /**
+   * 帳號 demo@miniasp.com
+   * 密碼 123456
+   */
+  user: UserLoginInfo = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor(private router: Router, private loginService: LoginService) {}
 
-  ngOnInit(): void {
+  login() {
+    this.loginService.login(this.user).subscribe({
+      // 可以有三個事件
+      // 1. next
+      next: (res) => {
+        localStorage.setItem('token', res.user.token);
+        // this.router.navigate(['/']);
+        this.router.navigateByUrl('/');
+      },
+      // 2. error
+      error: (error: HttpErrorResponse) => {
+        console.log(error);
+      },
+      // 3. complete
+      complete: () => {},
+    });
   }
-
 }
