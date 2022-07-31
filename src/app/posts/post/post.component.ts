b5fc9780ad7c8e92932857a37199e5afc3377eeb
kimi <kimi.lai@tpisoftware.com>
2022-07-31 13:40:30 +0800

    chore: 任務 03：在程式中取得路由參數

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
+import { Article } from 'src/app/interfaces/article';
+import { PostService } from 'src/app/post.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
+  id!: string;
+  article?: Article;
+  constructor(
+    private postService: PostService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        map((paramMap) => paramMap.get('id') || ''),
+        switchMap((id) => this.postService.getArticle(id))
+      )
+      .subscribe({
+        next: (res) => {
+          this.article = res.article;
+        },
+      });
   }
-
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Article } from '../interfaces/article';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
+  articles!: Article[];
 
-  constructor() { }
+  articles$: Observable<Article[]> = this.postService
+    .getArticles()
+    .pipe(map((result) => result.articles));
+
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
+    this.articles$.subscribe();
+    // this.postService.getArticles().subscribe({
+    //   next: (res) => {
+    //     this.articles = res.articles;
+    //   },
+    // });
   }
-
 }
